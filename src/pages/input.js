adeb04ff6e6fbfac6e68dd7286e2120a9d252f88
David Wong <davidwong@ps.asl.com.hk>
2023-09-05 11:23:28 +0800

    adding back padding to Input component

--- src/pages/input.js
+++ src/pages/input.js
@@ -2,7 +2,7 @@ import InputBox from "@/components/InputBox";
 
 const Input = () => {
     return (
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: "flex", alignItems: "center", padding: 20 }}>
             <InputBox />
             <div style={{ marginLeft: 10 }}>Please do not remove me</div>
         </div>
