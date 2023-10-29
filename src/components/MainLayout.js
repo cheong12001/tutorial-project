61ffc9f1236633dad82a93677450469c53cd810b
David Wong <davidwong@ps.asl.com.hk>
2023-10-30 01:56:02 +0800

    update

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,37 +2,52 @@ import { debounce } from "lodash";
 import { useEffect, useRef, useState } from "react";
 
 const MainLayout = ({ children }) => {
-
     const count = useRef(0);
     const [__, forceUpdate] = useState(Math.random());
+    const timer = useRef(0);
 
     const updateFunction = async () => {
-        count.current += 1;
-        await fetch("/api/data");
+        console.log(
+            "%c 111828 MainLayout.js enter this",
+            "background: green",
+            timer.current
+        );
+
+        if (timer.current) {
+            clearTimeout(timer.current);
+        }
+
+        timer.current = setTimeout(() => {
+            count.current += 1;
+            // await fetch("/api/data");
+            forceUpdate(Math.random());
+        }, 1000);
+
+        // fetch("/api/data");
     };
 
     const debounceFunction = debounce(updateFunction, 1000);
 
     useEffect(() => {
-        debounceFunction();
-        debounceFunction();
-        debounceFunction();
+        window.addEventListener("click", debounceFunction);
+        window.addEventListener("mousemove", debounceFunction);
 
-        setTimeout(() => {
-            forceUpdate(Math.random());
-        }, 2000);
+        return () => {
+            window.removeEventListener("click", debounceFunction);
+            window.addEventListener("mousemove", debounceFunction);
+        };
     }, []);
 
-    useEffect(()=>{
-        debounceFunction();
-        debounceFunction();
-
-    }, [])
+    // useEffect(() => {
+    //     debounceFunction();
+    //     debounceFunction();
+    // }, []);
 
     console.log(
         "%c 105014 MainLayout.js count.current",
         "background: green",
-        count.current
+        count.current,
+        timer.current
     );
 
     return <div>{children}</div>;
